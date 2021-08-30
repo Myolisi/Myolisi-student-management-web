583b60fd218233e1cf93422f68a97ee6f908c46d
myolisi <myolisi@ngubentombi.co.za>
2021-08-30 18:46:47 +0200

    Add student and service interfaces

---
+++ src/app/course.interface.ts
@@ -0,0 +1,6 @@
+export interface Course {
+  id?: number;
+  code: string;
+  description: string;
+  grade: string;
+}
---
+++ src/app/services/student.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Student } from '../../student.interface';
+import { throwError, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StudentService {
+  constructor(private http: HttpClient) {}
+  baseURL = 'http://localhost:8080/api';
+  any;
+  getStudents(): Observable<Student[] | any> {
+    return this.http
+      .get<Student[]>(`${this.baseURL}/student/all`)
+      .pipe(catchError(this.errorHandler));
+  }
+
+  uploadStudent(student: Student[]): Observable<Student[] | any> {
+    return this.http
+      .post<Student[]>(`${this.baseURL}/student/add`, student)
+      .pipe(catchError(this.errorHandler));
+  }
+
+  // handle error
+  errorHandler(error: any): any {
+    throwError(error.message);
+  }
+}
